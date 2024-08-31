9f96b3ec8634e5d181811137b0adcb0c25d3e0e9
Akshit Patel <sonaniakshit684@gmail.com>
2024-08-31 10:36:54 +0530

    Update 11.Primality testing prime or not.py

--- SEMESTER 1/PYTHON/11.Primality testing prime or not.py
+++ SEMESTER 1/PYTHON/11.Primality testing prime or not.py
@@ -1,1 +1,9 @@
-number=float(input("Enter any Number: "))
+number1=float(input("Enter any Number: "))
+number=int(number1)
+if(number>1):
+    for i in range(2,(number//2)+1):
+        if(number%i)==0:
+            print("Not Prime.")
+            break
+    else:
+        print("Prime.")
