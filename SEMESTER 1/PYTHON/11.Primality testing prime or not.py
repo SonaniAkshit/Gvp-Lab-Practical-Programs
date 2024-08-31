cef49fa3458ec0065a50e26b052331e0ba506538
Akshit Patel <sonaniakshit684@gmail.com>
2024-08-31 09:51:48 +0530

    Update 11.Primality testing prime or not.py

--- SEMESTER 1/PYTHON/11.Primality testing prime or not.py
+++ SEMESTER 1/PYTHON/11.Primality testing prime or not.py
@@ -1,18 +1,1 @@
-num = int(input("Enter a positive Number: "))
-
-if num <= 1:
-    print(num, "is not prime")
-elif num <= 3:
-    print(num, "is prime")
-else:
-    if num % 2 == 0 or num % 3 == 0:
-        print(num, "is not prime")
-    else:
-        i = 5
-        while i * i <= num:
-            if num % i == 0 or num % (i + 2) == 0:
-                print(num, "is not prime")
-                break
-            i += 6
-        else:
-            print(num, "is prime")
+number=float(input("Enter any Number: "))
