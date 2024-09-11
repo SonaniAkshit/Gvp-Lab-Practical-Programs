dd09712dc67c5dbd63b91c0fa9b087d7fb558366
Akshit Patel <sonaniakshit684@gmail.com>
2024-09-11 12:45:20 +0530

    Update 7.check card is lucky or not.py

--- Python/Assignment-1/7.check card is lucky or not.py
+++ Python/Assignment-1/7.check card is lucky or not.py
@@ -1,16 +1,15 @@
+print("\n")
 card = int(input("Enter Any Card Number: "))
 
 if card < 1 or card > 25:
-    print("-------------------")
+    print("\n")
     print("Invalid Card Number")
-    print("-------------------")
 else:
     lucky_numbers = {1, 3, 7, 9, 13, 15, 17, 19, 21, 23}
     if card in lucky_numbers:
-        print("------------------")
+        print("\n")
         print("The Card is lucky!")
-        print("------------------")
     else:
-        print("----------------------")
+        print("\n")
         print("The card is not lucky.")
-        print("----------------------")
+        print("\n")
