4074962f65af2a78995186a9e5dd3befbe8e2b1f
Akshit Patel <sonaniakshit684@gmail.com>
2024-09-11 12:44:57 +0530

    Update 2.find the sum of the first n natural numbers.py

--- Python/Assignment-1/2.find the sum of the first n natural numbers.py
+++ Python/Assignment-1/2.find the sum of the first n natural numbers.py
@@ -1,12 +1,12 @@
+print("\n")
 n = input("Enter a Positive Numbers: ")
 
 if n.isdigit() and int(n) >= 0:
     m = int(n)
     sum = m * (m + 1) // 2
-    print("------------------------------------------------")
+    print("\n")
     print("The Sum of The First", m, "Positive Integers is:", sum)
-    print("------------------------------------------------")
 else:
-    print("------------------------------------------------")
+    print("\n")
     print("Invalid Input ! Please Enter a Positive Numbers.")
-    print("------------------------------------------------")
+    print("\n")
