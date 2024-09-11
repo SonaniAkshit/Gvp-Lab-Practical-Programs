e9532c9f3cf7a2123fb06016712ede75cba91e2d
Akshit Patel <sonaniakshit684@gmail.com>
2024-09-11 12:45:16 +0530

    Update 6.Find the student from cs department.py

--- Python/Assignment-1/6.Find the student from cs department.py
+++ Python/Assignment-1/6.Find the student from cs department.py
@@ -1,29 +1,25 @@
+print("\n")
 roll = input("Enter roll number : ")
 
 if len(roll) != 9:
-    print("------------------------------------------------------------")
+    print("\n")
     print("Invalid roll number. enter roll number is 9 characters long.")
-    print("------------------------------------------------------------")
 elif not roll[0:4].isdigit():
-    print("----------------------------------------------------------")
+    print("\n")
     print("Invalid roll number. first four number represent the year.")
-    print("----------------------------------------------------------")
 elif not roll[4:6].isalpha():
-    print("------------------------------------------------------------------------------")
+    print("\n")
     print("Invalid roll number. next two characters alphabetic represent department name.")
-    print("------------------------------------------------------------------------------")
 elif not roll[6:].isdigit():
-    print("----------------------------------------------------------")
+    print("\n")
     print("Invalid roll number. last number represent student number.")
-    print("----------------------------------------------------------")
 elif roll[4:6]=='CS' or roll[4:6]=='cs':
     year = roll[0:4]
     dept_code = roll[4:6]
     stu_num = roll[6:]
 
-    print("----------------------------------------------")
+    print("\n")
     print("This Student From Computer Science Department.")
-    print("----------------------------------------------")
 
     # print("------------------------")
     # print("Year:", year)
@@ -31,9 +27,9 @@ elif roll[4:6]=='CS' or roll[4:6]=='cs':
     # print("Student Number:", stu_num)
     # print("------------------------")
 else:
-    print("-------------------")
+    print("\n")
     print("another department.")
-    print("-------------------")
+    print("\n")
 
 # if department_code == "CS":
 #         print("Computer Science department.")
