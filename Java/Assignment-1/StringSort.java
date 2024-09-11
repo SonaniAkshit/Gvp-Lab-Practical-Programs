9301cb5e79108743cbfb33adffcbc33cf51727a6
Akshit Patel <sonaniakshit684@gmail.com>
2024-09-11 12:44:38 +0530

    Update StringSort.java

--- Java/Assignment-1/StringSort.java
+++ Java/Assignment-1/StringSort.java
@@ -1,34 +1,3 @@
-/*import java.util.*;
-
-class StringSort 
-{
-    public static void main(String args[]) 
-	{
-        if (args.length == 0) 
-		{
-            System.out.println("Please provide some integers as command-line arguments.");
-            return;
-        }
-
-        for (int i = 0; i < args.length - 1; i++) 
-		{
-            for (int j = 0; j < args.length - 1 - i; j++) 
-			{
-                if (args[j].compareTo(args[j + 1]) > 0) 
-				{
-                    String temp = args[j];
-                    args[j] = args[j + 1];
-                    args[j + 1] = temp;
-                }
-            }
-        }
-        System.out.println("Sorted Strings:");
-        for (String str : args) 
-		{
-            System.out.println(str);
-        }
-    }
-}*/
 import java.util.*;
 
 class StringSort
